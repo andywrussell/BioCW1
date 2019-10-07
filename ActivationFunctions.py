8d0f89e77763a317b35c5524d859dec1d978aad2
Andrew Russell <andrewwrussell1@outlook.com>
2019-10-07 18:34:22 +0100

    Tried to implement activation functions

---
+++ ActivationFunctions.py
@@ -0,0 +1,17 @@
+import math
+
+def NullActivator(in_val) : 
+    return 0
+
+def SigmoidActivator(in_val) :
+    return 1 / (1 - math.exp(-in_val))
+
+def HyperbolicActivator(in_val) : 
+    return math.tanh(in_val)
+
+def CosineActivator(in_val) :
+    return math.cos(in_val)
+
+##This one doesnt work
+def GaussianActivator(in_val) : 
+    return math.exp(-((in_val**2)/2))
