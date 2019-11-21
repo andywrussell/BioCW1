e67c29889781b1648f2629fda3078e49763a3e05
wanderdust <pablo.lopez.santori@gmail.com>
2019-11-21 19:27:42 +0000

    Add Experiment class to run experiments

---
+++ experiments.py
@@ -0,0 +1,58 @@
+from ANN.networkGenerator import NetworkGenerator
+from PSO.pso import PSO
+from utils.helpers import MSE, read_data
+import os
+
+"""
+Class Experiment takes some parameters and runs a pso.
+"""
+class Experiment:
+    def __init__(self, pso_params, net_params, path, net_generator=NetworkGenerator, PSO=PSO):
+        self.pso_params = pso_params
+        self.net_params = net_params
+        self.path = path
+
+        self.pso = None
+        self.network = None
+
+        self.load_data(self.path)
+        self.build_network(net_generator)
+        self.build_pso(PSO)
+
+
+
+    def build_network(self, net_generator):
+        self.network = net_generator()
+        
+        for key, val in self.net_params.items():
+            self.network.add_layer(
+                input_count=val["input_count"],
+                node_count=val["node_count"], activations=[])
+    
+    def build_pso(self, PSO):
+        assert len(self.network.layers) != 0, "Network must be initialized before initializing the pso"
+
+        self.pso = PSO(
+            net_generator = self.network,
+            swarmsize = self.pso_params["swarmsize"],
+            alpha = self.pso_params["alpha"],
+            beta = self.pso_params["beta"],
+            gamma = self.pso_params["gamma"],
+            delta = self.pso_params["delta"],
+            jumpsize = self.pso_params["jumpsize"],
+            boundary = self.pso_params["boundary"],
+            num_informants = self.pso_params["num_informants"],
+            max_runs = self.pso_params["max_runs"],
+            ideal=self.ideal,
+            inputs=self.inputs)
+
+    def load_data(self, path):
+        current_dir = os.getcwd() + '/'
+        self.inputs, self.ideal = read_data(current_dir, path)
+
+    def print_results(self):
+        print("Show some metrics here!")
+
+    def run(self):
+        self.pso.run_algo()
+        self.print_results()
--- main.py
+++ main.py
@@ -4,36 +4,43 @@
 from ANN.neuralNet import NeuralNet
 from ANN.layer import Layer
 from ANN.networkGenerator import NetworkGenerator
+from experiments import Experiment
 from PSO.pso import PSO
 import numpy as np
 import pandas as pd
-import os
 from utils.helpers import MSE, read_data
 import os
 
-
-swarmsize = 100
-alpha = 1
-beta = 1
-gamma = 1
-delta = 1
-jumpsize = 0.5
-#ideal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-boundary = 5
-num_informants = 10
-max_runs = 2
-
-# Shape of our neural net
-net_generator = NetworkGenerator()
-net_generator.add_layer(input_count=2 , node_count=4)
-net_generator.add_layer(input_count=4 , node_count=1)
-
 current_dir = os.getcwd() + '/'
-inputs, ideal = read_data(current_dir, "2in_complex.txt")
+inputs, ideal = read_data(current_dir, "2in_xor.txt")
 
-my_pso = PSO(net_generator, swarmsize, alpha, beta, gamma, delta, jumpsize, ideal, inputs, num_informants, max_runs, boundary)
-my_pso.run_algo()
 
-best_fitness = my_pso.best.outputs
-print(my_pso.best.best_fitness)
+params_pso = {
+    "swarmsize": 60,
+    "alpha": 1,
+    "beta": 1,
+    "gamma": 1,
+    "delta": 1,
+    "jumpsize": 0.5,
+    "ideal": inputs,
+    "inputs": ideal,
+    "boundary": 5,
+    "num_informants": 10,
+    "max_runs": 30
+}
+
+net_layers = {
+    "layer1": {
+        "input_count":2,
+        "node_count":4,
+        "activations": []
+    },
+    "layer2": {
+        "input_count":4,
+        "node_count": 1,
+        "activations:":[]
+    }
+}
+
+experiment1 = Experiment(params_pso, net_layers, path="2in_xor.txt")
+experiment1.run()
