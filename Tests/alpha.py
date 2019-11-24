5c61e5c6818c3534a71bac873f5b119633d9ebea
wanderdust <pablo.lopez.santori@gmail.com>
2019-11-24 09:48:42 +0000

    Add 3 different architectures. 1 simple and 2 complex

--- Tests/alpha.py
+++ Tests/alpha.py
@@ -6,28 +6,7 @@ from PSO.pso import PSO
 import numpy as np
 import pandas as pd
 from utils.helpers import MSE, read_data
-from Tests.networks.net_architectures import architectures
-import os
-
-#Swarmsize - Start with 50 - typical is 10 - 100 according to lecture slides
-#alpha - start a 1
-#beta, gamma, delta typically sum to 4
-#acorrding to https://cs.gmu.edu/~sean/book/metaheuristics/Essentials.pdf delta is often set to 0
-#explained on page 57
-#gamma is often midpoint between delts and beta
-#start with weight boundary between -1 and 1
-#initially set informants to 3 according to page 5 of http://clerc.maurice.free.fr/pso/SPSO_descriptions.pdf
-
-#Run the baseline expriment on each data set to see results
-
-from ANN.neuralNet import NeuralNet
-from ANN.layer import Layer
-from ANN.networkGenerator import NetworkGenerator
-from experiments import Experiment
-from PSO.pso import PSO
-import numpy as np
-import pandas as pd
-from utils.helpers import MSE, read_data
+from Tests.networks.net_architectures import net_simple, net_complex_1, net_complex_2
 import os
 
 #Swarmsize - Start with 50 - typical is 10 - 100 according to lecture slides
@@ -69,7 +48,7 @@ def run_alpha():
         cubic_best = None
         for i in range(0, 10):      
             params_pso["alpha"] += 10
-            experiment1 = Experiment(params_pso, architectures["1in_cubic"], path="1in_cubic.txt", debugMode=False, sampleMode=True)
+            experiment1 = Experiment(params_pso, net_simple, path="1in_cubic.txt", debugMode=False, sampleMode=True)
             experiment1.run()
             if (cubic_best == None or experiment1.pso.best.fitness < cubic_best):
                 cubic_best = experiment1.pso.best.fitness
@@ -88,7 +67,7 @@ def run_alpha():
         linear_best = None
         for i in range(0, 10):      
             params_pso["alpha"] += 10
-            experiment1 = Experiment(params_pso, architectures["1in_linear"], path="1in_linear.txt", debugMode=False, sampleMode=True)
+            experiment1 = Experiment(params_pso, net_simple, path="1in_linear.txt", debugMode=False, sampleMode=True)
             experiment1.run()
             if (linear_best == None or experiment1.pso.best.fitness < linear_best):
                 linear_best = experiment1.pso.best.fitness
@@ -107,7 +86,7 @@ def run_alpha():
         sine_best = None
         for i in range(0, 10):      
             params_pso["alpha"] += 10
-            experiment1 = Experiment(params_pso, architectures["1in_sine"], path="1in_sine.txt", debugMode=False, sampleMode=True)
+            experiment1 = Experiment(params_pso, net_simple, path="1in_sine.txt", debugMode=False, sampleMode=True)
             experiment1.run()
             if (sine_best == None or experiment1.pso.best.fitness < sine_best):
                 sine_best = experiment1.pso.best.fitness
@@ -126,7 +105,7 @@ def run_alpha():
         tanh_best = None
         for i in range(0, 10):      
             params_pso["alpha"] += 10
-            experiment1 = Experiment(params_pso, architectures["1in_tanh"], path="1in_tanh.txt", debugMode=False, sampleMode=True)
+            experiment1 = Experiment(params_pso, net_simple, path="1in_tanh.txt", debugMode=False, sampleMode=True)
             experiment1.run()
             if (tanh_best == None or experiment1.pso.best.fitness < tanh_best):
                 tanh_best = experiment1.pso.best.fitness
@@ -146,7 +125,7 @@ def run_alpha():
         complex_best = None
         for i in range(0, 10):      
             params_pso["alpha"] += 10
-            experiment1 = Experiment(params_pso, architectures["2in_complex"], path="2in_complex.txt", debugMode=False, sampleMode=True)
+            experiment1 = Experiment(params_pso, net_complex_1, path="2in_complex.txt", debugMode=False, sampleMode=True)
             experiment1.run()
             if (complex_best == None or experiment1.pso.best.fitness < complex_best):
                 complex_best = experiment1.pso.best.fitness
@@ -165,7 +144,7 @@ def run_alpha():
         xor_best = None
         for i in range(0, 10):      
             params_pso["swarmsize"] += 10
-            experiment1 = Experiment(params_pso, architectures["2in_xor"], path="2in_xor.txt", debugMode=False, sampleMode=True)
+            experiment1 = Experiment(params_pso, net_complex_2, path="2in_xor.txt", debugMode=False, sampleMode=True)
             experiment1.run()
             if (xor_best == None or experiment1.pso.best.fitness < xor_best):
                 xor_best = experiment1.pso.best.fitness
--- Tests/networks/net_architectures.py
+++ Tests/networks/net_architectures.py
@@ -1,79 +1,38 @@
-architectures = {
-    "1in_cubic": {
-        "layer1": {
-            "input_count":1,
-            "node_count":2,
-            "activations": []
-        },
-        "layer2": {
-            "input_count":2,
-            "node_count": 1,
-            "activations:":[]
-        }
+net_simple = {
+    "layer1": {
+        "input_count":1,
+        "node_count":2,
+        "activations": []
     },
+    "layer2": {
+        "input_count":2,
+        "node_count": 1,
+        "activations:":[]
+    }
+}
 
-    "1in_linear" : {
-        "layer1": {
-            "input_count":1,
-            "node_count":2,
-            "activations": []
-        },
-        "layer2": {
-            "input_count":2,
-            "node_count": 1,
-            "activations:":[]
-        }
+net_complex_1 = {
+    "layer1": {
+        "input_count":2,
+        "node_count":2,
+        "activations": []
     },
+    "layer2": {
+        "input_count":2,
+        "node_count": 1,
+        "activations:":[]
+    }
+}
 
-    "1in_sine":  {
-        "layer1": {
-            "input_count":1,
-            "node_count":2,
-            "activations": []
-        },
-        "layer2": {
-            "input_count":2,
-            "node_count": 1,
-            "activations:":[]
-        }
+net_complex_2 = {
+    "layer1": {
+        "input_count":2,
+        "node_count":2,
+        "activations": []
     },
-
-    "1in_tanh" : {
-        "layer1": {
-            "input_count":1,
-            "node_count":2,
-            "activations": []
-        },
-        "layer2": {
-            "input_count":2,
-            "node_count": 1,
-            "activations:":[]
-        }
-    },
-
-    "2in_complex" : {
-        "layer1": {
-            "input_count":1,
-            "node_count":2,
-            "activations": []
-        },
-        "layer2": {
-            "input_count":2,
-            "node_count": 1,
-            "activations:":[]
-        }
-    },
-
-    "2in_xor" : {
-        "layer1": {
-            "input_count":1,
-            "node_count":2,
-            "activations": []
-        },
-        "layer2": {
-            "input_count":2,
-            "node_count": 1,
-            "activations:":[]
-        }
+    "layer2": {
+        "input_count":2,
+        "node_count": 1,
+        "activations:":[]
     }
 }
